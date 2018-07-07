1b8d5892de5f6e07337f0e7b76c34f43e3f4311f
haswelliris <haswelliris@live.com>
2018-07-08 01:19:38 +0800

    修改开发模式监听端口为3999 修改后端API请求方式

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // here, you can config the axios
-axios.defaults.baseURL = "https://cnjs.asmodeus.cn/api/v1";
+axios.defaults.baseURL = "http://cnjs.asmodeus.cn/api/v1";
 axios.create({
   timeout: 5000
 });
